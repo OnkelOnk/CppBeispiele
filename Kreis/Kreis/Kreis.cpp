2a19b2e0427cdc42b055a8a216ccbfcc0eca4f68
Ronja <Dirk.Ossenberg-E@posteo.de>
2016-04-09 20:14:44 +0200

    Änderung verworfen

--- Kreis/Kreis/Kreis.cpp
+++ Kreis/Kreis/Kreis.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 //Berechnet Basis hoch exponent
-//Dirk hat auch eine anederung die keinen Sinn macht.
 int exponentialrechnung(int basis, int exponent)
 {
 	int ergebniss = basis;
