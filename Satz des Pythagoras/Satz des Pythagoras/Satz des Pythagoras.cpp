c66e131e1d03ab10302b2e4b974355dafc1031f3
OnkelOnk <Dirk.Ossenberg-E@posteo.de>
2016-04-14 00:45:15 +0200

    Hilfe bei Fehler

--- Satz des Pythagoras/Satz des Pythagoras/Satz des Pythagoras.cpp
+++ Satz des Pythagoras/Satz des Pythagoras/Satz des Pythagoras.cpp
@@ -16,7 +16,27 @@ int exponentialrechnung(int basis, int exponent)
 }
 
 int main()
-{// Dirk was ist hier falsch, ich kann die Strecke b nicht eingeben.
+{
+	// Dirk was ist hier falsch, ich kann die Strecke b nicht eingeben.
+	/*
+	Hallo Ronja. Du hast den falschen Datentyp genommen. Der Datentyp bool kennt nur die Werte True und False bzw. 
+	0 und 1. Wenn du jetzt eine Zahl eingiebt die von 0 oder 1 verschienden ist kommt das Programm nicht damit zurecht 
+	weil es eine andere Eingabe erwartet. Das ist wie als wenn ich dich bitte Kartoffeln zu schälen und dir statt einem 
+	Schälmesser ein Wasserglas in die Hand gebe. Du waerst auch verwirrt und könntest die Aufgabe nicht erledigen. 
+	Bis wir dazu kommen warum er die zeite Eingabe ueberspringt kommt erst spaeter. 
+
+	Wir haten bisher folgende Datentypen:
+	int : Ganze Zahlen sowohl negativ als auch positiv. z.B. 1, -5, -56. 
+	double : Kommazahlen dowohl negativ als auch positiv: 3,2532.
+	bool : kennt nur true (englisch fuer Wahr) und false (englisch fuer falsch) oder auch 0 und 1.
+	char : Einzelner Buchstabe aus dem alphabet z.B. s,t,i
+
+	Es ist ganz wichtig, dass man immer den richtigen Datentyp nimmt. Versuch es mit den Infos nochmal die Aufgabe zu lösen. 
+	Lass dich nicht entmutigen. Diese Fehler sind am Anfang ganz normal. :). Schau auch mal auf die Variable Ergebniss und deren Datentyp.
+
+	Wenn du die Aufgabe bis zum Wochende schaffst können wir gerne wieder telefonieren und ich zeige dir neue Sachen. 
+	*/
+
 	bool Streckea;
 	cout << "Bitte geben sie die Strecke a ein:";
 	cin >> Streckea;
