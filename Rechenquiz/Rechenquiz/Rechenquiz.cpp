2edfa4fdcb0be6e39602cde5afdc9860592b00f8
OnkelOnk <Dirk.Ossenberg-E@posteo.de>
2016-04-18 14:11:45 +0200

    Anmerkungen zu dem neuen Prgramm

--- Rechenquiz/Rechenquiz/Rechenquiz.cpp
+++ Rechenquiz/Rechenquiz/Rechenquiz.cpp
@@ -25,7 +25,7 @@ double Rechnung1(double Zahl1, double Zahl2){
 			cout << "Du hast eine zu kleine Zahl eingegeben." << endl;
 		}
 		if (beispiel > Zahl1 + Zahl2){
-			cout << "du hast eine zu große Zahl engegeben." << endl;
+			cout << "du hast eine zu groÃŸe Zahl engegeben." << endl;
 		}
 		weitermachen = true;
 		Punkte = Punkte - 5;
@@ -54,7 +54,7 @@ double Rechnung2(double Zahl1, double Zahl2) {
 				cout << "Du hast eine zu kleine Zahl eingegeben." << endl;
 			}
 			if (beispiel > Zahl1 - Zahl2) {
-				cout << "du hast eine zu große Zahl engegeben." << endl;
+				cout << "du hast eine zu groÃŸe Zahl engegeben." << endl;
 			}
 			weitermachen = true;
 			Punkte = Punkte - 5;
@@ -83,7 +83,7 @@ double Rechnung3(double Zahl1, double Zahl2) {
 				cout << "Du hast eine zu kleine Zahl eingegeben." << endl;
 			}
 			if (beispiel > Zahl1 * Zahl2) {
-				cout << "du hast eine zu große Zahl engegeben." << endl;
+				cout << "du hast eine zu groÃŸe Zahl engegeben." << endl;
 			}
 			weitermachen = true;
 			Punkte = Punkte - 5;
@@ -111,7 +111,7 @@ double Rechnung4(double Zahl1, double Zahl2) {
 				cout << "Du hast eine zu kleine Zahl eingegeben." << endl;
 			}
 			if (beispiel > Zahl1 / Zahl2) {
-				cout << "du hast eine zu große Zahl engegeben." << endl;
+				cout << "du hast eine zu groÃŸe Zahl engegeben." << endl;
 			}
 			weitermachen = true;
 			Punkte = Punkte - 5;
@@ -123,7 +123,21 @@ double Rechnung4(double Zahl1, double Zahl2) {
 }
 
 int main()
-{
+{ /*Hier beginnt der Block der main Funktion*/
+	
+	/*
+	Hallo Ronja. Das sieht schon echt gut aus hat aber noch Fehler. Die Variable Punkte wird nicht richtig verwendet. 
+	Das liegt an den sogenannten Gueltigkeitsbereichen. Eine Variable ist nur innerbalb ihres Blocks und in allen 
+	Bloecken die dieser enthaelt definiert. Ein Block ist alles was in diesen Klammern steht: {}.
+	Das bedeutet, dass die Variable int Punkte in dieser Funktion auch nur in dieser Funktion bekannt ist. Du hast eine Variable 
+	mit dem gleichen Namen in deinen vier Funktionen. Diese haben den Gleichen Namen sind aber immer eine andere Variable, die nur 
+	innerhalb dieser Funktion bekannt ist. 
+	Die Idee die Punkte als Rueckgabewert der Funktion zu machen ist sehr gut. Dann muss der Aufruf aber wiefolgt ausehen: 
+	Punkte = Punkte + Rechnung1(64, 58);
+	Dadurch wirde der Rueckgabewert korrekt gespeichert. Du hast wieder die Datentypen etwas durcheinder gebracht. Schau da nochmal 
+	drauf. 
+	*/
+	
 	int Punkte = 0;
 	Rechnung1(64, 58);
 	Rechnung4(27, 3);
@@ -131,6 +145,9 @@ int main()
 	Rechnung2(56, 47);
 	Rechnung3(5, 7);
 	if (Punkte >= 40) {
+		/*
+		Das ist ein Unterblock der Funktion main. Die Variable int Punkte ist hier also auch bekannt. 
+		*/
 		Rechnung1(48, 37);
 		Rechnung2(36, 6);
 		Rechnung4(48, 8);
@@ -139,5 +156,5 @@ int main()
 	}
 	system("pause");
     return 0;
-}
+/*Hier endet der Block der main Funktion*/}
 
